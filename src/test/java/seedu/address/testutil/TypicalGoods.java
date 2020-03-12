7fd54260df5d9f159f5f79ecc18e9732b04a1b0e
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-12 16:44:55 +0800

    Add Json for Good and Inventory with relevant tests.

--- src/test/java/seedu/address/testutil/TypicalGoods.java
+++ src/test/java/seedu/address/testutil/TypicalGoods.java
@@ -15,11 +15,16 @@ import seedu.address.model.good.Good;
  */
 public class TypicalGoods {
 
-    public static final Good APPLE = new GoodBuilder().withGoodName("Fuji apple")
-            .withGoodQuantity(15).build();
+    public static final Good APPLE = new GoodBuilder().withGoodName("Fuji apple").withGoodQuantity(15).build();
 
-    public static final Good BANANA = new GoodBuilder().withGoodName("Brazil banana")
-            .withGoodQuantity(10).build();
+    public static final Good BANANA = new GoodBuilder().withGoodName("Cavendish banana").withGoodQuantity(150).build();
+    public static final Good CITRUS = new GoodBuilder().withGoodName("Brazil citrus").withGoodQuantity(20).build();
+    public static final Good DURIAN = new GoodBuilder().withGoodName("Malaysia durian").withGoodQuantity(11).build();
+    public static final Good ENTAWAK = new GoodBuilder().withGoodName("Indonesia entawak").withGoodQuantity(0).build();
+
+    // Manually added
+    public static final Good FIG = new GoodBuilder().withGoodName("India fig").withGoodQuantity(10).build();
+    public static final Good GRAPE = new GoodBuilder().withGoodName("Spain grape").withGoodQuantity(10).build();
 
     // Manually added - Good's details found in {@code CommandTestUtil}
     public static final Good AVOCADO = new GoodBuilder().withGoodName(VALID_GOOD_NAME_AVOCADO).build();
@@ -37,7 +42,6 @@ public class TypicalGoods {
     }
 
     public static List<Good> getTypicalGoods() {
-        return new ArrayList<>(Arrays.asList(APPLE, BANANA));
+        return new ArrayList<>(Arrays.asList(APPLE, BANANA, CITRUS, DURIAN, ENTAWAK));
     }
-
 }
