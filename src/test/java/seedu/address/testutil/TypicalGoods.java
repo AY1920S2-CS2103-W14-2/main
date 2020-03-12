460e538935b0f84532486be20cef7f96184b07c2
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-12 16:14:04 +0800

    Add Inventory and related tests.

--- src/test/java/seedu/address/testutil/TypicalGoods.java
+++ src/test/java/seedu/address/testutil/TypicalGoods.java
@@ -1,5 +1,13 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GOOD_NAME_AVOCADO;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GOOD_NAME_BLUEBERRY;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.Inventory;
 import seedu.address.model.good.Good;
 
 /**
@@ -12,5 +20,24 @@ public class TypicalGoods {
 
     public static final Good BANANA = new GoodBuilder().withGoodName("Brazil banana")
             .withGoodQuantity(10).build();
+
+    // Manually added - Good's details found in {@code CommandTestUtil}
+    public static final Good AVOCADO = new GoodBuilder().withGoodName(VALID_GOOD_NAME_AVOCADO).build();
+    public static final Good BLUEBERRY = new GoodBuilder().withGoodName(VALID_GOOD_NAME_BLUEBERRY).build();
+
+    /**
+     * Returns an {@code Inventory} with all the typical goods.
+     */
+    public static Inventory getTypicalInventory() {
+        Inventory inventory = new Inventory();
+        for (Good person : getTypicalGoods()) {
+            inventory.addGood(person);
+        }
+        return inventory;
+    }
+
+    public static List<Good> getTypicalGoods() {
+        return new ArrayList<>(Arrays.asList(APPLE, BANANA));
+    }
 
 }
