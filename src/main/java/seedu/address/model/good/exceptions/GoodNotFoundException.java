2c329b334acdef11a4074e5ba88b7e86c58e6a9a
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-15 16:16:26 +0800

    Fix design choices and coding style errors.

--- src/main/java/seedu/address/model/good/exceptions/GoodNotFoundException.java
+++ src/main/java/seedu/address/model/good/exceptions/GoodNotFoundException.java
@@ -4,4 +4,7 @@ package seedu.address.model.good.exceptions;
  * Signals that the operation is unable to find the specified good.
  */
 public class GoodNotFoundException extends RuntimeException {
+    public GoodNotFoundException() {
+        super("Operation can not find the specific good");
+    }
 }
