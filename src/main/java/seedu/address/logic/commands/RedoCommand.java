eec2486af329c34ab49dba64ecae925366841b3b
NicholasCF <nicholas.fernando@u.nus.edu>
2020-03-29 20:40:53 +0800

    Implement RedoCommand execute

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.StateNotFoundException;
 
 /**
  * Returns to a version of the application before the last execution of an UndoCommand.
@@ -14,6 +16,11 @@ public class RedoCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        try {
+            model.redo();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (StateNotFoundException e) {
+            throw new CommandException(Messages.MESSAGE_REDO_AT_LATEST_STATE);
+        }
     }
 }
